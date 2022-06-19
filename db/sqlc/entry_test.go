d5fd9056445153733baf91c72be877248dbd72f6
Phumthawan Nokun <phumtawan.nokun@oivan.com>
2022-06-20 00:21:32 +0700

    add entry test

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,3 +1,77 @@
 package db
 
-//TODO: create entry test
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	amount := int64(10)
+	acc := createRandomAccount(t)
+
+	arg := CreateEntryParams{
+		AccountID: acc.ID,
+		Amount:    amount,
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, acc.ID)
+	require.Equal(t, arg.Amount, amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t)
+	}
+
+	arg := ListEntriesParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	acc, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, acc, 5)
+
+	for _, v := range acc {
+		require.NotEmpty(t, v)
+	}
+}
