92b25b3b76d2dd3641fa978e7322734e1daaf35e
Phumthawan Nokun <phumtawan.nokun@oivan.com>
2022-07-02 18:54:56 +0700

    set config

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 
 	"github.com/Mersock/golang-sample-bank/api"
 	db "github.com/Mersock/golang-sample-bank/db/sqlc"
+	"github.com/Mersock/golang-sample-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:P@ssword@localhost:5433/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("connat load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("server down", err)
 	}
