f682c63e21814ac45f3347ffb5755a53a737c9c6
Phumthawan Nokun <phumtawan.nokun@oivan.com>
2022-07-02 17:08:03 +0700

    add server

---
+++ api/accounts.go
@@ -0,0 +1,87 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	db "github.com/Mersock/golang-sample-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountReq struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency"  binding:"required,oneof=USD EUR"`
+}
+
+type getAccountReq struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type listAccountReq struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errRes(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errRes(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountReq
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errRes(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errRes(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errRes(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountReq
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errRes(err))
+		return
+	}
+
+	arg := db.ListAccountParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	account, err := server.store.ListAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errRes(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
---
+++ api/server.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/Mersock/golang-sample-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+
+	router.GET("/ping", pingRes)
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func pingRes(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func errRes(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
+}
