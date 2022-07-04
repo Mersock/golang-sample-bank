413345d3d1a9a328979c14a5f9dc932acd62f7d7
Phumthawan Nokun <phumtawan.nokun@oivan.com>
2022-07-04 09:30:30 +0700

    fix wrong name

--- util/password.go
+++ util/password.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func HassPassword(password string) (string, error) {
+func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", fmt.Errorf("failed to hash password %w", err)
