a64c3aba00dc003137977d7a99529ec367fa7a79
phumthawan.nokun <phumthawan.nokun@siriustech.io>
2022-12-29 22:05:00 +0700

    add validation for grpc

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -6,12 +6,19 @@ import (
 	db "github.com/Mersock/golang-sample-bank/db/sqlc"
 	"github.com/Mersock/golang-sample-bank/pb"
 	"github.com/Mersock/golang-sample-bank/util"
+	"github.com/Mersock/golang-sample-bank/val"
 	"github.com/lib/pq"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserReq) (*pb.CreateuserRes, error) {
+	violations := validateCreateuserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	hashPassword, err := util.HashPassword(req.GetPassword())
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
@@ -42,4 +49,23 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserReq) (*p
 		User: convertUser(user),
 	}
 	return res, nil
+}
+
+func validateCreateuserRequest(req *pb.CreateUserReq) (violation []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violation = append(violation, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violation = append(violation, fieldViolation("password", err))
+	}
+
+	if err := val.ValidateFullname(req.GetFullName()); err != nil {
+		violation = append(violation, fieldViolation("full_name", err))
+	}
+
+	if err := val.ValidateEmail(req.GetFullName()); err != nil {
+		violation = append(violation, fieldViolation("email", err))
+	}
+	return violation
 }
--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,12 +7,19 @@ import (
 	db "github.com/Mersock/golang-sample-bank/db/sqlc"
 	"github.com/Mersock/golang-sample-bank/pb"
 	"github.com/Mersock/golang-sample-bank/util"
+	"github.com/Mersock/golang-sample-bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginReq) (*pb.LoginRes, error) {
+	violations := validateLogineuserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUsers(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -67,4 +74,16 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginReq) (*pb.Logi
 	}
 
 	return res, nil
+}
+
+func validateLogineuserRequest(req *pb.LoginReq) (violation []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violation = append(violation, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violation = append(violation, fieldViolation("password", err))
+	}
+
+	return violation
 }
